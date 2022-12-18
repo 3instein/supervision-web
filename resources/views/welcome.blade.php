ca01ae3b3185ff64e3a7e8017c2826208ce23fc9
ssabrut <michael.gunawan2002@gmail.com>
2022-12-18 11:43:11 +0700

    Change card menu and continue mockup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
       <i data-feather="shopping-cart"></i>
     </div>
     <div class="flex">
-      <select class="flex-auto rounded-xl" name="" id="">
+      <select class="flex-auto rounded-xl border-gray-400" name="" id="">
         <option value="">Filter</option>
       </select>
-      <button class="inline-flex border rounded-xl ml-4 items-center justify-center px-4" type="button"><i data-feather="search"></i>Search</button>
+      <button class="inline-flex border border-gray-400 rounded-xl ml-4 items-center justify-center px-4 text-xs" type="button"><i class="w-4 h-4 mr-2" data-feather="search"></i>Search</button>
     </div>
   </nav>
   <div>
@@ -21,36 +21,36 @@
         <img class="h-[143px] object-cover"
           src="https://images.unsplash.com/photo-1512058564366-18510be2db19?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80"
           alt="" />
-        <div class="mt-4 ml-4">
-          <p>Nasi Goreng</p>
-          <p class="pb-4">Rp. 10,000</p>
+        <div class="mt-4 mx-4">
+          <p class="font-light">Nasi Goreng</p>
+          <p class="pb-4 text-xs font-extrabold">Rp. 10,000</p>
         </div>
       </div>
       <div class="w-[164px] border mb-4 rounded-xl overflow-hidden">
         <img class="h-[143px] object-cover"
-          src="https://images.unsplash.com/photo-1512058564366-18510be2db19?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80"
+          src="https://images.unsplash.com/photo-1541832676-9b763b0239ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1320&q=80"
           alt="" />
-        <div class="mt-4 ml-4">
-          <p>Nasi Goreng</p>
-          <p class="pb-4">Rp. 10,000</p>
+        <div class="mt-4 mx-4">
+          <p class="font-light">Nasi Hainan</p>
+          <p class="pb-4 text-xs font-extrabold">Rp. 30,000</p>
         </div>
       </div>
       <div class="w-[164px] border mb-4 rounded-xl overflow-hidden">
         <img class="h-[143px] object-cover"
-          src="https://images.unsplash.com/photo-1512058564366-18510be2db19?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80"
+          src="https://images.unsplash.com/photo-1615937657715-bc7b4b7962c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
           alt="" />
-        <div class="mt-4 ml-4">
-          <p>Nasi Goreng</p>
-          <p class="pb-4">Rp. 10,000</p>
+        <div class="mt-4 mx-4">
+          <p class="font-light">Wagyu Tomahawk</p>
+          <p class="pb-4 text-xs font-extrabold">Rp. 180,000</p>
         </div>
       </div>
       <div class="w-[164px] border mb-4 rounded-xl overflow-hidden">
         <img class="h-[143px] object-cover"
-          src="https://images.unsplash.com/photo-1512058564366-18510be2db19?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80"
+          src="https://images.unsplash.com/photo-1598866594230-a7c12756260f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1308&q=80"
           alt="" />
-        <div class="mt-4 ml-4">
-          <p>Nasi Goreng</p>
-          <p class="pb-4">Rp. 10,000</p>
+        <div class="mt-4 mx-4">
+          <p class="font-light">Spaghetti Bolognase</p>
+          <p class="pb-4 text-xs font-extrabold">Rp. 35,000</p>
         </div>
       </div>
     </div>
