0a6dd6bcc14495d089d12df659a5b6bdbdeb54ca
3instein <reynaldi_kindarto@yahoo.com>
2022-12-19 09:05:18 +0700

    Sign in feature for customer

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,13 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\Authenticatable;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
-
-class Customer extends Authenticatable
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Authenticatable as AuthenticableTrait;
+class Customer extends Model implements Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+
+
+    use AuthenticableTrait;
 
     protected $fillable = [
         'email',
