ecfdc87b1d143d60f58da885b6e49cb902fd4fb3
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2023-01-07 18:02:58 +0700

    Add tier in vouchers

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,13 +4,14 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Tier;
 use App\Models\User;
 use App\Models\Order;
 use App\Models\Store;
 use App\Models\Table;
+use App\Models\Voucher;
 use App\Models\Customer;
 use App\Models\Transaction;
-use App\Models\Voucher;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Database\Seeders\MenuSeeder;
@@ -23,6 +24,24 @@ class DatabaseSeeder extends Seeder {
      * @return void
      */
     public function run() {
+        Tier::create([
+            'name' => 'Bronze',
+            'description' => 'Bronze Tier',
+            'image' => 'bronze.png',
+            'required_points' => 5000,
+        ]);
+        Tier::create([
+            'name' => 'Silver',
+            'description' => 'Silver Tier',
+            'image' => 'silver.png',
+            'required_points' => 10000,
+        ]);
+        Tier::create([
+            'name' => 'Gold',
+            'description' => 'Gold Tier',
+            'image' => 'gold.png',
+            'required_points' => 15000,
+        ]);
         Store::create([
             'name' => 'Test Store',
             'address' => 'Jl. Test',
@@ -47,33 +66,16 @@ class DatabaseSeeder extends Seeder {
                 'points' => 0
             ]
         );
-        // $order = Order::create(
-        //     [
-        //         'customer_id' => 1,
-        //         'table_id' => 1,
-        //     ]
-        // );
 
-        // $order->menus()->attach(1, ['quantity' => 1, 'note' => 'Test Note']);
         Voucher::create(
             [
+                'tier_id' => 1,
                 'name' => 'Test Voucher',
                 'description' => 'Test Voucher',
                 'minimal' => 10000,
                 'discount' => 10000,
             ]
         );
-        // Transaction::create([
-        //     'order_id' => 1,
-        //     'voucher_id' => 1,
-        //     'confirmed_by' => null,
-        //     'total' => 10000,
-        //     'payment_method' => 'Cash',
-        //     'status' => 'Unpaid',
-        // ]);
 
-
-
-        
     }
 }
